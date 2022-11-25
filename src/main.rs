88dff7412d9670f13abe3ee0eef4932ce61e3f00
Axzurey <66486198+Axzurey@users.noreply.github.com>
2022-11-24 21:39:28 -0500

    clones electron

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{self, BufRead, Write};
+use std::io::{self, BufRead, Write, Cursor};
 
 use utils::{make_file, clone_repo};
 pub mod utils;
@@ -9,30 +9,24 @@ struct Dust {
 }
 //template: https://github.com/Axzurey/electron-react-typescript-webpack-template
 
-//MODIFY: package.json, 
 struct CreateElectronApp {
     name: String,
     author: String,
     desc: String
 }
 
-fn create_electron_app() {
-    let mut line = String::new();
+fn create_electron_app(additional: Additional) {
     let stdin = io::stdin();
 
     let table = ["name", "author", "desc"];
     let queries = ["Set an application name", "Set the author", "Set a description"];
-    let mut results: [String; 3] = [String::from(""), String::from(""), String::from("")];
+    let mut results: [String; 3] = ["".to_string(), "".to_string(), "".to_string()];
     let mut pointer = 0;
 
     loop {
         print!("{}\t", queries[pointer]);
         io::stdout().flush().expect("An unexpected error occured!");
-        stdin.lock().read_line(&mut line).expect("Unable to read line");
-
-        let clone = line.clone();
-
-        results[pointer] = clone;
+        stdin.lock().read_line(&mut results[pointer]).expect("Unable to read line");
 
         pointer += 1;
 
@@ -41,10 +35,10 @@ fn create_electron_app() {
 
     let _res = clone_repo(&String::from("https://github.com/Axzurey/electron-react-typescript-webpack-template"), &results[0]);
 
-
+    let file = std::fs::read_to_string(file!()); //https://docs.rs/tar/latest/tar/
 }
 
-fn dust_file() {
+fn dust_file(additional: Additional) {
     let out = Dust {
         file_name: std::env::args().nth(2).expect("Argument 2 is missing (File Name)"),
         content: std::env::args().nth(3).unwrap_or("".to_string())
@@ -53,20 +47,27 @@ fn dust_file() {
     make_file(&out.file_name, &out.content).expect("Error creating file");
 }
 
-fn construct_param_from_cli() {
+fn construct_param_from_cli(additional: Additional) {
     let cmd = std::env::args().nth(1).expect("Argument 1 is missing (Command)");
 
     if cmd == String::from("dust") {
-        dust_file();
+        dust_file(additional);
     }
     else if cmd == String::from("create-electron-app") {
-        create_electron_app();
+        create_electron_app(additional);
     }
     else {
         panic!("{}", format!("{cmd} is not a valid command!"))
     }
 }
 
+struct Additional<'a> {
+    electron: Cursor<&'a [u8; 5396992]>
+}
+
 fn main() {
-    let _cli = construct_param_from_cli();
+    let electrapp = Cursor::new(include_bytes!("create-electron-app.tar"));
+    let _cli = construct_param_from_cli(Additional {
+        electron: electrapp
+    });
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::Write, path::Path};
+use std::{fs::File, io::Write, path::{Path, PathBuf}};
 use git2::Repository;
 
 
@@ -14,16 +14,12 @@ pub fn make_file(path: &String, content: &String) -> Result<(), std::io::Error> 
 }
 
 pub fn clone_repo(url: &String, path: &String) -> git2::Repository {
-    let repo = match Repository::init(Path::new(&path)) {
-        Ok(r) => {
-            match Repository::clone(&url, Path::new(&path)) {
-                Ok(repo) => repo,
-                Err(e) => panic!("{}", format!("Unable to clone repo {url} to {path} because {e}")),
-            };//https://docs.rs/git2/latest/git2/struct.Repository.html
-            r
-        },
+    let res = path.as_str().trim();
+    std::fs::create_dir(res).expect("unable to create path");
+    let repo = match Repository::clone(&url, res) {
+        Ok(repo) => repo,
         Err(e) => panic!("{}", format!("Unable to clone repo {url} to {path} because {e}")),
-    };
+    };//https://docs.rs/git2/latest/git2/struct.Repository.html
 
     return repo;
 }
